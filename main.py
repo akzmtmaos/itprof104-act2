52635adc8cf377f09c803a5b1b46c459277190ae
akzmtmaos <134627851+akzmtmaos@users.noreply.github.com>
2024-03-21 09:58:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import pyttsx3
+import speech_recognition as sr
+
+def talk(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+    engine.say(text)
+    engine.runAndWait()
+
+listener = sr.Recognizer()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if "jacob" in command:
+                if "cat noise" in command: # Hey Jacob, can you make cat noise?
+                    response = "I don't know but... let me try....! Meow! Meow!.... Did it work?"
+                elif "friends" in command: # Hey Jacob, do you have friends?
+                    response = "I do have friends who are also AI. They are Siri, Bard, and Alexa"
+                elif "artist" in command: # Hey Jacob, do you know any artist?
+                    response = "I do know few artists. Mostly I know Taylor Swift. I am such a big fan"
+                else:
+                    response = "Sorry, I didn't catch that. Can you repeat?"
+
+                print(response)
+                talk(response)
+    except:
+        pass
+        command = "No Mic"
+        print(command)
+
+take_command()
